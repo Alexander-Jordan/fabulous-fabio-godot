9f3fe649a5726cb6e54b1d743043fc6332510b07
Alexander-Jordan <curtjordan93@gmail.com>
2025-09-17 07:25:55 +0200

    First basic level layout, and add crouching to fabio

--- entities/fabio/fabio.gd
+++ entities/fabio/fabio.gd
@@ -4,7 +4,7 @@ class_name Fabio extends CharacterBody2D
 ## Greatest fall speed.
 const FALL_SPEED_MAX = 1200
 ## The force applied when jumping.
-const JUMP_FORCE = 2000
+const JUMP_FORCE = 1800
 ## How long the player can increase their jump by holding down the jump button.
 const JUMP_TIME: float = 0.25
 ## Speed value to add every frame.
@@ -16,6 +16,13 @@ const RUN_SPEED_MAX = 150
 #region VARIABLES
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var animation: String = 'idle':
+	set(a):
+		if a == animation or !['crouch', 'die', 'idle', 'jump', 'run', 'slide'].has(a):
+			return
+		animation = a
+		animated_sprite_2d.animation = a
+var crouching: bool = false
 var direction: float = 0.0:
 	set(d):
 		if d == direction:
@@ -28,6 +35,11 @@ var jump_time: float = 0.0
 
 #region FUNCTIONS
 func _process(delta: float) -> void:
+	crouching = Input.is_action_pressed('down')
+	if crouching:
+		direction = 0.0
+		return
+	
 	direction = Input.get_axis('left', 'right')
 	
 	# initial jump force, applied only when grounded:
@@ -43,28 +55,32 @@ func _process(delta: float) -> void:
 		jump_time = 0.0
 
 func _physics_process(delta: float) -> void:
-	if (direction > 0.0 and velocity.x < RUN_SPEED_MAX) or (direction < 0.0 and velocity.x > -RUN_SPEED_MAX):
+	if !crouching and (direction > 0.0 and velocity.x < RUN_SPEED_MAX) or (direction < 0.0 and velocity.x > -RUN_SPEED_MAX):
 		velocity.x += direction * RUN_SPEED_AMPLIFIER * delta
 	if direction == 0.0:
 		velocity.x -= signf(velocity.x) * RUN_SPEED_AMPLIFIER * delta
 		if velocity.x < 0.01 and velocity.x > -0.01:
 			velocity.x = 0.0
 	
+	if crouching:
+		animation = 'crouch'
+		if !is_on_floor() and velocity.y < FALL_SPEED_MAX:
+			velocity.y += (get_gravity().y * 3) * delta
+		move_and_slide()
+		return
+	
 	if is_on_floor():
 		if velocity.x != 0.0:
-			if direction != 0.0 and signf(direction) != signf(velocity.x) and absf(velocity.x) > 0.1:
-				animated_sprite_2d.animation = 'slide'
+			if direction != 0.0 and signf(direction) != signf(velocity.x):
+				animation = 'slide'
 			else:
-				animated_sprite_2d.animation = 'run'
-				animated_sprite_2d.speed_scale = (absf(velocity.x) / RUN_SPEED_MAX) * 2.0
-				animated_sprite_2d.speed_scale = clampf(animated_sprite_2d.speed_scale, 0.0, 2.0)
+				animation = 'run'
 		else:
-			animated_sprite_2d.animation = 'idle'
+			animation = 'idle'
 	else:
-		animated_sprite_2d.animation = 'jump'
-	
-	if !is_on_floor() and velocity.y < FALL_SPEED_MAX:
-		velocity.y += get_gravity().y * delta
+		animation = 'jump'
+		if velocity.y < FALL_SPEED_MAX:
+			velocity.y += get_gravity().y * delta
 	
 	move_and_slide()
 #endregion
