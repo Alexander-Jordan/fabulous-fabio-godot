ed4a7c401cb774ea7fd89399d320b7fdd3d7f54d
Alexander-Jordan <curtjordan93@gmail.com>
2025-10-26 15:12:21 +0100

    Make it possible to get health from mistery boxes

--- entities/crates/mystery_crate/mystery_crate.gd
+++ entities/crates/mystery_crate/mystery_crate.gd
@@ -1,1 +1,15 @@
 class_name MysteryCrate extends Crate
+
+var item_type: int = randi_range(0, 1)
+
+func spawn_item(spawn_point: Vector2) -> void:
+	if items_count <= 0:
+		return
+	
+	var direction: Vector2 = Vector2(randf_range(-100, 100), randf_range(-300, -100))
+	if item_type == 0:
+		CS.call_deferred('spawn', spawn_point, direction)
+	else:
+		HS.call_deferred('spawn', spawn_point, direction)
+	
+	items_count -= 1
