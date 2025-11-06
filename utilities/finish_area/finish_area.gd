5e58f4253f59bec863fa8c775d09733fa7e8d1f3
Alexander-Jordan <curtjordan93@gmail.com>
2025-11-06 22:53:32 +0100

    Award points depending on how far the jump at the finish

--- utilities/finish_area/finish_area.gd
+++ utilities/finish_area/finish_area.gd
@@ -5,4 +5,10 @@ func _ready() -> void:
 
 func on_body_entered(body: Node2D) -> void:
 	if body is Fabio:
+		# award 100 points for every 10px the player jumped
+		var distance: float = body.global_position.distance_to(global_position)
+		var points: int = int(distance / 10) * 100
+		SS.stats.score += points
+		FTS.spawn(body.global_position, str(points))
+		
 		body.finished = true
