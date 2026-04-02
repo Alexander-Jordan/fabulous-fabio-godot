47c10ba6ab1cb8d60771c77eb62136d0e1c48bdd
Alexander-Jordan <curtjordan93@gmail.com>
2026-04-02 14:49:30 +0200

    Make crates work differently depending on gravity direction

--- entities/crates/crate.gd
+++ entities/crates/crate.gd
@@ -24,16 +24,27 @@ var disabled: bool = false:
 func _ready() -> void:
 	area_2d.body_entered.connect(on_area_2d_body_entered)
 
-func animate_crate_bump() -> void:
+func animate_crate_bump(gravity_shifted: bool) -> void:
 	var tween = create_tween()
-	tween.tween_property(animated_sprite_2d, 'position:y', -5, 0.02)
+	var final_val: float = 5 if gravity_shifted else -5
+	tween.tween_property(animated_sprite_2d, 'position:y', final_val, 0.02)
 	tween.tween_property(animated_sprite_2d, 'position:y', 0, 0.1)
 
 func on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Fabio:
-		AS.spawn(global_position, audio_bump)
-		animate_crate_bump()
-		spawn_item(Vector2(global_position.x, global_position.y - 10))
+	if body is not Fabio:
+		return
+	
+	# super complicated way of determining if fabio is "below" the crate
+	# depending on the up_direction of fabio (influenced by gravity direction)
+	var gravity_shifted = body.up_direction.y > 0
+	var fabio_below = global_position.y > body.global_position.y if gravity_shifted else global_position.y < body.global_position.y
+	if !fabio_below:
+		return
+	
+	AS.spawn(global_position, audio_bump)
+	animate_crate_bump(gravity_shifted)
+	var y_position: float = global_position.y + 10 if gravity_shifted else global_position.y - 10
+	spawn_item(Vector2(global_position.x, y_position))
 
 func spawn_item(spawn_point: Vector2) -> void:
 	if items_count <= 0:
